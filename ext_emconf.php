c68f71f0dd82cb58e00de61dcda7f5a76790f052
divyagoklani-sudo <divya.goklani@mail.nitsan.ai>
2026-08-27 15:43:40 +0530

    Add files via upload

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF['ns_friendlycaptcha'] = [
+    'title' => 'TYPO3 Friendly Captcha',
+    'description' => 'Protect your TYPO3 forms from bots without annoying puzzles. Friendly Captcha runs seamlessly in the background and complies fully with GDPR regulations.', 
+    
+    'category' => 'plugin',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
+    'state' => 'stable',
+    'version' => '14.0.3',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-14.9.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
